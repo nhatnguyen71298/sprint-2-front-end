81c2d371f9622fffde72a233cad00bd426fc43b0
nhatprovklno3 <nguyenhongnhat71298@gmail.com>
2021-01-05 14:44:59 +0700

    Update fire-base 2

---
+++ sprint2-frontend/src/app/app-admin/parking-slot/list-parking-slot/list-parking-slot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-parking-slot',
+  templateUrl: './list-parking-slot.component.html',
+  styleUrls: ['./list-parking-slot.component.css']
+})
+export class ListParkingSlotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
