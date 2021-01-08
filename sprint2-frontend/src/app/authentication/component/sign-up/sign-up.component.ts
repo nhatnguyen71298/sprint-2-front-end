ba57e1d49ae26fe503f91d001ae0557a3240a4f5
Your Name <khanh06111993@gmail.com>
2021-01-08 10:09:54 +0700

    08/01/2021 frontend 10h07

---
+++ sprint2-frontend/src/app/authentication/component/sign-up/sign-up.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../service/auth/authentication.service';
+
+function comparePassword(c: AbstractControl): any {
+  const v = c.value;
+  const isNotEmpty = v.confirmPassword !== '';
+  if (isNotEmpty) {
+    return (v.passwordNew === v.confirmPassword) ? null : {
+      passwordNotMatch: true
+    };
+  }
+}
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
+  checkRegister: boolean;
+  messageError = '';
+
+  constructor(
+    public formBuilder: FormBuilder,
+    // public loginService: LoginService,
+    private authenticationService: AuthenticationService,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.pattern('^[a-z][a-z0-9_\\.]{3,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,2}$')]],
+      passwordNew: ['', [Validators.required, Validators.pattern(/^[a-z0-9A-Z]{6,30}$/)]],
+      confirmPassword: ['', [Validators.required]],
+    }, {validator: comparePassword});
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid){
+      this.authenticationService.register(this.signUpForm.value.username, this.signUpForm.value.password ).subscribe( next => {
+        this.checkRegister = next;
+        if (this.checkRegister === true){
+          this.router.navigateByUrl('/');
+        }else {
+          this.messageError = 'Tên tài khoản đã tồn tại. Vui lòng nhập tên khác.';
+        }
+      });
+    } else {
+      this.messageError = 'Bạn đang để trống hoặc sai thông tin , vui lòng điền đầy đủ thông tin.';
+    }
+  }
+}
